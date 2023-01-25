c67e225554d6478514b7b54c2feda0194516b40a
trende2001 <68961063+trende2001@users.noreply.github.com>
2023-01-25 17:27:14 +0100

    Tower Resort Scoreboard (#1)

--- code/UI/BaseHud.cs
+++ code/UI/BaseHud.cs
@@ -7,17 +7,18 @@ using Sandbox;
 using Sandbox.UI;
 using Components.BaseGamesUi;
 using Components.NotificationManager;
+using Sandbox.UI;
 using TowerResort.UI;
 
 public class BaseHud : RootPanel
 {
 	public static BaseHud Current;
 	public NotificationManagerUI NotificationManager;
-	public BaseGamesUi.PlayersPanel playersPanel;
+	/*public BaseGamesUi.CardsPanel TestCardsPanel;*/
 
 	public BaseHud()
 	{
-		if (Current != null)
+		if ( Current != null )
 		{
 			Current.Delete();
 			Current = null;
@@ -31,7 +32,6 @@ public class BaseHud : RootPanel
 		
 		AddChild<TRChat>();
 		AddChild<HudTracker>();
-		//TEMPORARY
-		AddChild<Scoreboard<ScoreboardEntry>>();
+		AddChild<Scoreboard>();
 	}
 }
