4cec054a7cfa2d9679c4b7d6e3c16d116dcf7ff7
ItsRifter <d_thomas_smith30@hotmail.com>
2023-01-27 22:07:37 +0000

    use VR template by Xezno (experiment)

---
+++ code/Pawn/Types/Lobby/VR/LobbyPawn.VR.LeftHand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerResort.Player.VR;
+
+public partial class LeftHand : VRHandEntity
+{
+	protected override string ModelPath => "models/hands/handleft.vmdl";
+	public override Input.VrHand InputHand => Input.VR.LeftHand;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		Log.Info( "VR Controller Right Spawned" );
+		Tags.Add( "vrhand" );
+	}
+}
+
---
+++ code/Pawn/Types/Lobby/VR/LobbyPawn.VR.RightHand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerResort.Player.VR;
+
+public partial class RightHand : VRHandEntity
+{
+	protected override string ModelPath => "models/hands/handright.vmdl";
+	public override Input.VrHand InputHand => Input.VR.RightHand;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		Log.Info( "VR Controller Right Spawned" );
+		Tags.Add( "vrhand" );
+	}
+}
+
